3be8238e83fdc94d5d6a342091e93a61966e8839
Sam Johnson <cxfusion@gmail.com>
2013-05-13 16:18:37 +1000

    First commit. Libraries sorted.

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "OneWire/OneWire.h"
+#include "tempCtrl/DallasTemperature.h"
+#include "TSL2561/Adafruit_TSL2561.h"
+
+void setup()
+{
+	Serial1.begin(115200);
+
+}
+
+void loop()
+{
+	delay(2000);
+}
+
+void initADH8066()
+{
+
+}
